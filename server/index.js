ce80f416572d7e8e4ebc0a028b9b72ce52a664ab
Denny Min <denden223@gmail.com>
2021-09-07 18:00:46 -0700

    Started the post request server response. - Denny Min

--- server/index.js
+++ server/index.js
@@ -13,3 +13,11 @@ app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`express server listening on port ${process.env.PORT}`);
 });
+
+app.use(express.json());
+app.get('/app/new/workout', (req, res) => {
+  const payload = 'hello sexy';
+  console.log(req.body);
+  res.status(200).json(payload);
+}
+);
