491e4a9fa7d5458ee41aff21333de7b4ebcaf4bf
Denny Min <denden223@gmail.com>
2021-09-10 17:47:37 -0700

    Was able to add a listing for my fitness which should open up an accordion for new workout or a new meal. - Denny Min

--- client/components/temp-drawer.jsx
+++ client/components/temp-drawer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Drawer, IconButton, makeStyles, Typography } from '@material-ui/core';
+import { Drawer, IconButton, makeStyles, Button, List, ListItem } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 
 const drawerWidth = 150;
@@ -11,6 +11,17 @@ const useStyles = makeStyles(theme => {
     },
     drawerPaper: {
       width: drawerWidth
+    },
+    positioning: {
+      position: 'absolute',
+      top: 20
+    },
+    listFont: {
+      fontFamily: 'Roboto',
+      padding: 0
+    },
+    fullWidth: {
+      width: '100%'
     }
   };
 });
@@ -21,8 +32,23 @@ export default function TempDrawer() {
   const toggleDrawer = e => {
     state === true ? setState(false) : setState(true);
   };
+
+  const list = () => {
+    return (
+    <div>
+      <List>
+        <ListItem className={classes.listFont}>
+          <Button className={classes.fullWidth}>
+            My Fitness
+          </Button>
+        </ListItem>
+      </List>
+    </div>
+    );
+  };
+
   return (
-    <>
+    <span className={classes.positioning}>
       <IconButton
         onClick={e => toggleDrawer(e)}
       >
@@ -35,8 +61,8 @@ export default function TempDrawer() {
       classes={{ paper: classes.drawerPaper }}
       onClose={e => toggleDrawer(e)}
       >
-        <Typography onClick={e => toggleDrawer(e)} onKeyDown={e => toggleDrawer(e)}>Hi</Typography>
+        {list()}
       </Drawer>
-    </>
+    </span>
   );
 }
