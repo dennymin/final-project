73510b8bfb3595c36df714065f2e4e4439c7ce4e
Denny Min <denden223@gmail.com>
2021-09-13 18:05:43 -0700

    Removed the random function into the return statement. - Denny Min

--- client/components/temp-drawer.jsx
+++ client/components/temp-drawer.jsx
@@ -29,60 +29,7 @@ export default function TempDrawer() {
 
   const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
 
-  const list = () => {
-    return (
-    <>
-      <List>
-        <ListItem className={classes.listFont}>
-          <Accordion square={true} expanded={true}>
-            <AccordionSummary>
-              My Fitness
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                <Link
-                  href='/#app/your/workouts'
-                  underline='none'
-                  color='textPrimary'
-                  onClick={e => setIsDrawerOpen(!isDrawerOpen)}
-                >
-                  Your Workouts
-                </Link>
-              </Typography>
-            </AccordionDetails>
-            <AccordionDetails>
-              <Typography>
-                <Link
-                  href='/#app/new/workout'
-                  underline='none'
-                  color='textPrimary'
-                  onClick={e => setIsDrawerOpen(!isDrawerOpen)}
-                >
-                  New Workout
-                </Link>
-              </Typography>
-            </AccordionDetails>
-            <AccordionDetails>
-              <Typography>
-                <Link
-                  href='/#app/new/meal'
-                  underline='none'
-                  color='textPrimary'
-                  onClick={e => setIsDrawerOpen(!isDrawerOpen)}
-                >
-                  New Meal
-                </Link>
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-        </ListItem>
-      </List>
-    </>
-    );
-  };
-
   return (
-  // <ThemeProvider theme = { theme }>
       <span className={classes.positioning}>
         <IconButton
           onClick={e => setIsDrawerOpen(!isDrawerOpen)}
@@ -96,9 +43,52 @@ export default function TempDrawer() {
         classes={{ paper: classes.drawerPaper }}
           onClose={e => setIsDrawerOpen(!isDrawerOpen)}
         >
-          {list()}
+        <List>
+          <ListItem className={classes.listFont}>
+            <Accordion square={true} expanded={true}>
+              <AccordionSummary>
+                My Fitness
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  <Link
+                    href='/#app/your/workouts'
+                    underline='none'
+                    color='textPrimary'
+                    onClick={e => setIsDrawerOpen(!isDrawerOpen)}
+                  >
+                    Your Workouts
+                  </Link>
+                </Typography>
+              </AccordionDetails>
+              <AccordionDetails>
+                <Typography>
+                  <Link
+                    href='/#app/new/workout'
+                    underline='none'
+                    color='textPrimary'
+                    onClick={e => setIsDrawerOpen(!isDrawerOpen)}
+                  >
+                    New Workout
+                  </Link>
+                </Typography>
+              </AccordionDetails>
+              <AccordionDetails>
+                <Typography>
+                  <Link
+                    href='/#app/new/meal'
+                    underline='none'
+                    color='textPrimary'
+                    onClick={e => setIsDrawerOpen(!isDrawerOpen)}
+                  >
+                    New Meal
+                  </Link>
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+          </ListItem>
+        </List>
         </Drawer>
-        </span>
-  // </ThemeProvider>
+      </span>
   );
 }
