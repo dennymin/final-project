217a5269eff23d6b337845e611680f01799c3ada
Denny Min <denden223@gmail.com>
2021-09-18 00:51:19 -0700

    Setting up how the contacts will be presented. I was trying to utilize filter but was getting stuck. - Denny Min

--- client/pages/contacts.jsx
+++ client/pages/contacts.jsx
@@ -1,8 +1,49 @@
-import { Card, CardContent, Grid, Typography } from '@material-ui/core';
-import React from 'react';
+import { Card, CardContent, Grid, Typography, List, ListItem } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/header';
 
 export default function Contacts(props) {
+  const [userList, pullUsersList] = useState([{
+    firstName: '',
+    lastName: ''
+  }]);
+
+  useEffect(() => {
+    let isCanceled = false;
+    const serverAddress = '/api/others';
+    fetch(serverAddress, {
+      headers: {
+        'signin-token': window.localStorage.getItem('signin-token')
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        !isCanceled && pullUsersList(data);
+        const firstLetters = [];
+        for (let i = 0; i < userList.length; i++) {
+          const firstLetter = userList[i].firstName[0];
+          if (!firstLetters.includes(firstLetter)) {
+            firstLetters.push(firstLetter);
+          }
+        }
+      });
+    return () => { isCanceled = true; };
+  }, [userList]);
+
+  const contact = props => {
+    // const contactList = props.people;
+    // const renderedPeople = contactList.map(person => {
+
+    // });
+    const firstLetters = [];
+    for (let i = 0; i < userList.length; i++) {
+      const firstLetter = userList[i].firstName[0];
+      if (!firstLetters.includes(firstLetter)) {
+        firstLetters.push(firstLetter);
+      }
+    }
+  };
+
   return (
     <>
       <Header title='SOCIAL' />
@@ -20,9 +61,18 @@ export default function Contacts(props) {
         >
           <Card raised={true}>
             <CardContent>
-              <Typography>
-                Hi
-              </Typography>
+              <List>
+                <ListItem>
+                  <Typography>
+                    Denny
+                  </Typography>
+                </ListItem>
+                <ListItem divider={true} alignItems='flex-start'>
+                  <Typography>
+                    Denny
+                  </Typography>
+                </ListItem>
+              </List>
             </CardContent>
           </Card>
         </Grid>
