0c9486bf68774a88827ef0a9eb2813a371ffc481
Denny Min <denden223@gmail.com>
2021-09-22 11:56:51 -0700

    Changed two links and changed some dummy data. All set for deploying to heroku. - Denny Min

--- database/data.sql
+++ database/data.sql
@@ -1,18 +1,9 @@
 insert into "users" ("userId", "username", "firstName", "lastName", "hashedPassword")
-values (5, 'isaachsu', 'isaac', 'hsu', '098F6BCD4621D373CADE4E832627B4F6'),
-       (6, 'heesookim', 'heesoo', 'kim', '098F6BCD4621D373CADE4E832627B4F6'),
-       (7, 'paulpark', 'paul', 'park', '098F6BCD4621D373CADE4E832627B4F6'),
-       (8, 'danielhong', 'daniel', 'hong', '098F6BCD4621D373CADE4E832627B4F6'),
-       (9, 'mikeayala', 'mike', 'ayala', '098F6BCD4621D373CADE4E832627B4F6'),
-       (10, 'cdc', 'cdc', 'lkhjklh', '098F6BCD4621D373CADE4E832627B4F6'),
-       (11, 'dcdc', 'dcdc', 'dudette', '098F6BCD4621D373CADE4E832627B4F6'),
-       (12, 'ccddcdc', 'ccddcdc', 'min', '098F6BCD4621D373CADE4E832627B4F6'),
-       (13, 'acavb', 'acavb', 'johnson', '098F6BCD4621D373CADE4E832627B4F6'),
-       (14, 'asdbsd', 'asdbsd', 'dude', '098F6BCD4621D373CADE4E832627B4F6'),
-       (15, 'bsababs', 'bsababs', 'def', '098F6BCD4621D373CADE4E832627B4F6'),
-       (16, 'dddddd', 'dddddd', 'qwee', '098F6BCD4621D373CADE4E832627B4F6'),
-       (17, 'cdcdcdccdc', 'cdcdcdccdc', 'lkhjklh', '098F6BCD4621D373CADE4E832627B4F6'),
-       (18, 'abababababa', 'abababababa', 'dudette', '098F6BCD4621D373CADE4E832627B4F6');
+values (905, 'isaachsu', 'isaac', 'hsu', '098F6BCD4621D373CADE4E832627B4F6'),
+       (906, 'heesookim', 'heesoo', 'kim', '098F6BCD4621D373CADE4E832627B4F6'),
+       (907, 'paulpark', 'paul', 'park', '098F6BCD4621D373CADE4E832627B4F6'),
+       (908, 'danielhong', 'daniel', 'hong', '098F6BCD4621D373CADE4E832627B4F6'),
+       (909, 'mikeayala', 'mike', 'ayala', '098F6BCD4621D373CADE4E832627B4F6');
 
 insert into "muscleGroup" ("muscleId", "name")
 values (1, 'Chest'), (2, 'Back'), (3, 'Arms'), (4, 'Legs');
